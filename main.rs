efe23c4f4c299192be1a6299a80c07073e0ce848
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-11 21:06:43 -0300

    up

---
+++ main.rs
@@ -0,0 +1,21 @@
+fn main (){
+    //let name: &str = "fabio"; /*inmutavel*/
+   // let mut name2: &str = "fabio"; /*mutavel*/
+
+
+    //integer
+    let age:u64 = 9;
+    // u64  unsigned não suporta inteiros negativos
+    let age:i64 = 9;
+    // i64  signed  suporta inteiros do tipos negativos
+    //i16 pode estar entre -32768 e 32767
+
+
+    let peso = 6.7;
+    //f64:flutuante
+
+    let status:bool = false;
+
+    println!("{}",age);
+}
+
