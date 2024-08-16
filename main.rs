662909093bd68bab13a4b10e2be417ad131b2374
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-16 17:57:00 -0300

    input de dados

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+//in C #include<stdio.h>
+use std::io;
+
 fn main (){
     //let name: &str = "fabio"; /*inmutavel*/
    // let mut name2: &str = "fabio"; /*mutavel*/
@@ -16,6 +19,7 @@ fn main (){
 
     //let status:bool = false;
 
+   /*
     fn verify_numbers(number1:i32,number2:i32){
         if number1 > number2{
             println!("{} é maior que  que {}",number1,number2);
@@ -25,12 +29,27 @@ fn main (){
     }
 
     verify_numbers(25,37)
+    */
 
+    // input de dados
 
+    fn convert_to_int(data: &String) -> i32{
+        let input_data = data.trim().parse::<i32>().unwrap();
+        return  input_data
+    }
 
+    let mut number1 = String::new();
+    let mut get_number_1 =  io::stdin().read_line(&mut number1).expect("error ao ler os dados").to_string();
+    let mut number2 = String::new();
+    let mut get_number_2 = io::stdin().read_line(&mut number2).expect("error ao ler os dados").to_string();
 
-
-
+    let convert_number1 = convert_to_int(&get_number_1);
+    let convert_number2 = convert_to_int(&get_number_2);
+    if convert_number1 <= convert_number2 {
+        println!("é menor");
+    }else{
+        println!("não é menor");
+    }
 
 
 
