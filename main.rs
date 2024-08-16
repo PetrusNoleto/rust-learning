3f7a933d43dd88a21d5c67034513d26c9161c593
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-16 17:35:08 -0300

    if and else flow

--- main.rs
+++ main.rs
@@ -4,18 +4,38 @@ fn main (){
 
 
     //integer
-    let age:u64 = 9;
+    //let age:u64 = 9;
     // u64  unsigned não suporta inteiros negativos
-    let age:i64 = 9;
+    //let age:i64 = 9;
     // i64  signed  suporta inteiros do tipos negativos
     //i16 pode estar entre -32768 e 32767
 
 
-    let peso = 6.7;
+    //let peso = 6.7;
     //f64:flutuante
 
-    let status:bool = false;
+    //let status:bool = false;
 
-    println!("{}",age);
+    fn verify_numbers(number1:i32,number2:i32){
+        if number1 > number2{
+            println!("{} é maior que  que {}",number1,number2);
+        }else{
+            println!("{} nao é maior que que {}",number1,number2);
+        }
+    }
+
+    verify_numbers(25,37)
+
+
+
+
+
+
+
+
+
+
 }
+
+
 
